4f82fdb28165a91156259a1b42ecc7987b3de380
22009657 <22009657@EITX-N-00390.energa.loc>
2020-07-15 15:57:10 +0200

    @NoRepositoryBean

--- src/main/java/webSpringBootApp/dataBase/interfaces/StudentRepo.java
+++ src/main/java/webSpringBootApp/dataBase/interfaces/StudentRepo.java
@@ -1,10 +1,11 @@
 package webSpringBootApp.dataBase.interfaces;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import webSpringBootApp.entities.Students;
 
-
+@NoRepositoryBean
 public interface StudentRepo extends CrudRepository<Students, Integer> {
 
 }
--- src/main/java/webSpringBootApp/dataBase/interfaces/StudentRepoExt.java
+++ src/main/java/webSpringBootApp/dataBase/interfaces/StudentRepoExt.java
@@ -1,13 +1,14 @@
 package webSpringBootApp.dataBase.interfaces;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.query.Param;
 
 import webSpringBootApp.entities.Students;
 
+
+//	@NoRepositoryBean	- nie zostanie bean znaleziony w klasie UpdateStudentRunner
 public interface StudentRepoExt extends StudentRepo {
-
-
 	
 	@Query("select st from Students st where st.name = :name")
 	public Students getStudentByName(@Param("name") String name); 
